53cdb6ae6bdc2c0f495cc5e9577cb47fdd6feb4e
Chunke Yang <chunkyang@ebay.com>
2015-08-25 14:49:39 -0700

    tidyXml

--- bin/tidyXml.sh
+++ bin/tidyXml.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-XMLLINT_INDENT=$'  ' xmllint --format --encode utf-8 --output /tmp/$1_tidy $1 
-mv /tmp/$1_tidy $1
+echo xmllint --format --encode utf-8 --output /tmp/$1_$$ $1 
+XMLLINT_INDENT=$'  ' xmllint --format --encode utf-8 --output /tmp/$1_$$ $1 
+echo mv /tmp/$1_$$ $1
+mv /tmp/$1_$$ $1
